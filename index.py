386e2a57e9914defd0cffefa92c7f3f090b91546
jbrereton6 <jbrereton6@outlook.com>
2019-03-25 08:02:16 -0400

    did stuffs

--- index.py
+++ index.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('dashboard.html')
+
+@app.route('/fieldConfig')
+def fieldConfig():
+    return render_template('field.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
