c08167f0898972434968b8bc49088b849b53e18f
lgreif <lgreif27@gmail.com>
2019-04-24 17:36:38 -0400

    dashboard update

---
+++ socketio-test.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('dashboard.html')
+
+@app.route('/scoring')
+def scoring():
+    return render_template('scoring.html')
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+if __name__ == '__main__':
+    socketio.run(app)
